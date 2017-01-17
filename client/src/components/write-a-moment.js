65d28389e187a30560fe59281918cbd1b1e6a57a
jdobrowner <j@jasondobrowner.com>
2017-01-16 17:01:43 -0800

    simple routes and links

---
+++ client/src/components/write-a-moment.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+export default class Record_Moment extends Component {
+  constructor() {
+    super();
+    this.onHandleSubmit = this.onHandleSubmit.bind(this);
+  }
+  onHandleSubmit(event) {
+    event.preventDefault();
+    console.log('moment remebered');
+  }
+  render() {
+    return (
+      <div>
+        <h1>Write a Moment</h1>
+        <form onSubmit={this.onHandleSubmit}>
+          <label>
+            Your moment:
+            <textarea name="moment" rows="4" cols="30" />
+          </label>
+          <input type="submit" value="Remember" />
+        </form>
+      </div>
+    )
+  }
+}
