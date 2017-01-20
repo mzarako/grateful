340079b04e0f3abf14ac859f332e0451ba63859f
jdobrowner <j@jasondobrowner.com>
2017-01-20 06:58:12 -0800

    gitignore config

--- server/config/index.js
+++
@@ -1,9 +0,0 @@
-var configuration = {
-	express_port: 3090,
-	mogodb: {
-		url: 'mongodb://localhost:mindful-moments/db'
-	},
-	secret: 'bdyer826s4n3k48ahfl8js98pq71mz'
-}
-
-module.exports = configuration;
