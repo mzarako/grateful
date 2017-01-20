e1b51954f83deb7c9fc6fca0048e65229c33ff01
Monika Zarako-Dobrowner <monika.zarako@gmail.com>
2017-01-20 07:03:43 -0800

    hamburger menu implemented

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -7,29 +7,56 @@ import signout from '../actions/signout';
 class Header extends Component {
   constructor() {
     super();
+    this.state = { hamburgerOpen: false };
     this.onSignoutClicked = this.onSignoutClicked.bind(this);
     this.navBar = this.navBar.bind(this);
+    this.hamburgerClicked = this.hamburgerClicked.bind(this);
+    this.toggleHamburgerNav = this.toggleHamburgerNav.bind(this);
   }
   onSignoutClicked() {
     this.props.signout();
+    this.hamburgerClicked();
+  }
+  hamburgerClicked() {
+    this.setState({hamburgerOpen: !this.state.hamburgerOpen});
+  }
+  returnHamburger() {
+    if (this.state.hamburgerOpen) {
+      let hamburger = 
+      [<div className="hamburger1 animate-hamburger1"></div>,
+      <div className="hamburger2 animate-hamburger2"></div>,
+      <div className="hamburger3 animate-hamburger3"></div>];
+      return hamburger;
+    }
+    else {
+      let hamburger = 
+      [<div className="hamburger1"></div>,
+      <div className="hamburger2"></div>,
+      <div className="hamburger3"></div>];
+      return hamburger;
+    }  
+  }
+  toggleHamburgerNav() {
+    if (this.state.hamburgerOpen) {
+      return "nav-visible";
+    }
+    else return "nav-hidden";
   }
   navBar(isLoggedIn) {
     if (isLoggedIn) {
       return (
-       <nav>
-          <button type="button">
-              <span className="icon-bar"></span>
-              <span className="icon-bar"></span>
-              <span className="icon-bar"></span>
-          </button>
-          <div className="nav-div">
+       <nav className={this.toggleHamburgerNav()}>
             <ul>
               <Link to="/write-a-moment"><li>Write a Moment</li></Link>
               <Link to="/read-a-moment"><li>Read a Moment</li></Link>
               <Link to="/account"><li>Account</li></Link>
               <Link to="/"><li onClick={this.onSignoutClicked}>Sign Out</li></Link>
-            </ul>
-          </div>  
+              <div className="button-div">
+                <button type="button" onClick={this.hamburgerClicked}>
+                  {this.returnHamburger()}
+                </button>
+              </div>  
+            </ul>  
       </nav>
         )
     }
--- client/src/components/read-a-moment.js
+++ client/src/components/read-a-moment.js
@@ -9,12 +9,12 @@ export default class ReadMoment extends Component {
       <section>
         <h1>Read a Moment</h1>
         <div>
-          <div className="read-arrow">arrow</div>
+          <div className="read-arrow inline">arrow</div>
           <div className="read-moment">
             <p>January 3, 2017</p>
             <p>"Today was such a beautiful day. I feel so grateful to have gone to Sequoia and stood amongst the ancient giants."</p>
           </div>  
-          <div className="read-arrow">arrow</div>
+          <div className="read-arrow inline">arrow</div>
         </div>  
         <button type="button">Save changes</button>
         <button type="button">Delete</button>
