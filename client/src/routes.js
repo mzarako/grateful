352f647b5bf6a56772e3d228e1af3cc4dce05b46
Monika Zarako-Dobrowner <monika.zarako@gmail.com>
2017-01-21 15:55:44 -0800

    protected routes

--- client/src/routes.js
+++ client/src/routes.js
@@ -10,13 +10,14 @@ import Account from './components/account';
 import Footer from './components/footer';
 import Login from './components/auth/login';
 import Signup from './components/auth/signup';
+import RequireAuth from './components/auth/require-authentication';
 
 export default (
   <Route path="/" component={App}>
     <IndexRoute component={Home} />
-    <Route path="write-a-moment" component={WriteMoment} />
-    <Route path="read-a-moment" component={ReadMoment} />
-    <Route path="account" component={Account} />
+    <Route path="write-a-moment" component={RequireAuth(WriteMoment)} />
+    <Route path="read-a-moment" component={RequireAuth(ReadMoment)} />
+    <Route path="account" component={RequireAuth(Account)} />
     <Route path="login" component={Login} />
     <Route path="signup" component={Signup} />
   </Route>
